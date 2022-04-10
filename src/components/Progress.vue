8862141562314891633949977671e5797c38fbf5
Semih Ünal <semih1837@gmail.com>
2022-04-10 14:12:46 +0300

    Products App - (Adding - Listing)

---
+++ src/components/Progress.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="progress">
+        <div class="progress-bar bg-info" role="progressbar" :style="{ width: progressValue + '%' }" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+        {{ currentItem }}/10
+        </div>
+    </div>
+</template>
+
+<script>
+import { eventBus } from '../main';
+
+export default ({
+    data(){
+        return {
+            currentItem: null,
+            progressValue: null
+        }
+    },
+    created () {
+        eventBus.$on('progressBarUpdated', (productListCount) => {
+            this.currentItem = productListCount;
+            this.progressValue = productListCount * 10;
+        })
+    }
+})
+</script>
